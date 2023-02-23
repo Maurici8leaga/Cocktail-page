014782d9b99c5ea02f22c7d2b874800371698b88
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-02-22 22:35:04 -0400

    Feat: adding dependencies
    It was add new dependencies such as: axios , bootstrap icons. Also was add google fonts and colors css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./Global.css";
+
+const App = () => {
+	return (
+		<div>
+			<h1>ESTE ES UN H1</h1>
+
+			<h2>Este es un h2</h2>
+
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem debitis
+				libero quis itaque id? Non iusto sed veritatis vel a omnis amet tempora
+				similique cum repudiandae, optio doloribus. Quam, tenetur.
+			</p>
+		</div>
+	);
+};
+
+export default App;
